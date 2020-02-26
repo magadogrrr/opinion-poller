730632402436b7305963ee33d81e6cd66e116abe
torrence payseno <tpayseno@gmail.com>
2020-02-26 01:01:39 -0500

    add env PORT

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ app.get('/', (req, res, next) => {
   res.send({ hi: 'there' });
 });
 
-app.listen(5000, () => { console.log("opinion-poller is now listening on port 5000")});
+app.listen(PORT, () => { console.log("opinion-poller is now listening on port 5000")});
