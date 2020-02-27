0ee80640000935a54933ad5db8e67e2c2ccbf462
torrence payseno <tpayseno@gmail.com>
2020-02-27 18:54:57 -0500

    add proxy to oauth

--- src/service/passport.js
+++ src/service/passport.js
@@ -26,7 +26,8 @@ passport.deserializeUser( async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
     }, async (accessToken, refreshToken, profile, done) => {
       let user;
       try {
